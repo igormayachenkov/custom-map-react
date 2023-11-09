b9c7e3cb10cc1969bd124409cac9dec4a167e930
igormayachenkov <igormayachenkov@gmail.com>
2023-11-09 13:26:52 +0300

    movement works

--- src/Map.js
+++ src/Map.js
@@ -6,24 +6,37 @@ const Map = ()=>{
     const [walker,setWalker] = useState({path:[[10,20], [20,30], [50,50]]})
     const [x,y] = walker.path[walker.path.length-1]
     
-    //const trackPoints = " 20,20 40,25 60,40 80,120 120,140 200,180"
+    useEffect(()=>{
+        console.log(`Start timer`)
+        window.setInterval(()=>{
+            const [x,y] = walker.path[walker.path.length-1]
+            console.log(`timer ${x},${y}`)
+            walker.path.push([x+10,y+10])
+            setWalker({
+                path: walker.path
+            })
 
-    const markerStyle = {left: (x-MARKER_RADIUS)+"px", top: (y-MARKER_RADIUS)+"px"}
+        },3000)
 
-    
+    },[])
 
     //------------------------------------------------------------------------
     // RENDER
-    console.log(`=> Map`, walker.path)
+    const markerStyle = {left: (x-MARKER_RADIUS)+"px", top: (y-MARKER_RADIUS)+"px"}
+    const trackPoints = walker.path.reduce((str,[x,y])=>str+=` ${x},${y}`, "") //const trackPoints = walker.path - works once!
+    //const trackPoints = " 20,20 40,25 60,40 80,120 120,140 200,180"
+
+    console.log(`=> Map`, trackPoints)
 
     return <div id="map">
         walker: {walker.toString()}
 
         <svg height="100%" width="100%">
-            <polyline points={walker.path}
+            <polyline points={trackPoints}
             stroke="white" strokeWidth="3" fill="none"
             />
         </svg> 
+
         <svg height={2*(MARKER_RADIUS+1)} width={2*(MARKER_RADIUS+1)} style={markerStyle}>
             <circle cx={MARKER_RADIUS+1} cy={MARKER_RADIUS+1} r={MARKER_RADIUS} stroke="white" strokeWidth="2" fill="red" />
         </svg> 
